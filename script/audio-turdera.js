5b0b85ae5e72dd5db405268b72f7c2fc74c02cfe
Pancho Piarristeguy <francisco.piarristeguy@hotmail.com>
2021-03-01 10:05:12 -0300

    primer commit

---
+++ script/audio-turdera.js
@@ -0,0 +1,222 @@
+var turderaAudio1 = document.getElementById('turderaAudio1');
+var turderaPlayPauseBTN1 = document.getElementById('turderaPlayPauseBTN1');
+var turderaCount1 = 0;
+
+function turderaPlayPause1(){
+	if(turderaCount1  == 0){
+		turderaCount1  = 1;
+		turderaAudio1.play();
+		turderaPlayPauseBTN1.innerHTML = "||";
+	}else{
+		turderaCount1 = 0;
+		turderaAudio1.pause();
+		turderaPlayPauseBTN1.innerHTML = "►";
+	}
+}
+ 
+function turderaStop1(){
+	turderaPlayPause1()
+	turderaAudio1.pause();
+	turderaAudio1.currentTime = 0;
+	turderaPlayPauseBTN1.innerHTML = "►";
+}
+
+/* -------------------------------------------------------------- */
+
+var turderaAudio2 = document.getElementById('turderaAudio2');
+var turderaPlayPauseBTN2 = document.getElementById('turderaPlayPauseBTN2');
+var turderaCount2 = 0;
+
+function turderaPlayPause2(){
+	if(turderaCount2  == 0){
+		turderaCount2  = 1;
+		turderaAudio2.play();
+		turderaPlayPauseBTN2.innerHTML = "||";
+	}else{
+		turderaCount2 = 0;
+		turderaAudio2.pause();
+		turderaPlayPauseBTN2.innerHTML = "►";
+	}
+}
+ 
+function turderaStop2(){
+	turderaPlayPause2()
+	turderaAudio2.pause();
+	turderaAudio2.currentTime = 0;
+	turderaPlayPauseBTN2.innerHTML = "►";
+}
+
+/* -------------------------------------------------------------- */
+
+var turderaAudio3 = document.getElementById('turderaAudio3');
+var turderaoniPlayPauseBTN3 = document.getElementById('turderaPlayPauseBTN3');
+var turderaCount3 = 0;
+
+function turderaPlayPause3(){
+	if(turderaCount3  == 0){
+		turderaCount3  = 1;
+		turderaAudio3.play();
+		turderaPlayPauseBTN3.innerHTML = "||";
+	}else{
+		turderaCount3 = 0;
+		turderaAudio3.pause();
+		turderaPlayPauseBTN3.innerHTML = "►";
+	}
+}
+ 
+function turderaStop3(){
+	turderaPlayPause3()
+	turderaAudio3.pause();
+	turderaAudio3.currentTime = 0;
+	turderaPlayPauseBTN3.innerHTML = "►";
+}
+
+/* -------------------------------------------------------------- */
+
+var turderaAudio4 = document.getElementById('turderaAudio4');
+var turderaPlayPauseBTN4 = document.getElementById('turderaPlayPauseBTN4');
+var turderaCount4 = 0;
+
+function turderaPlayPause4(){
+	if(turderaCount4  == 0){
+		turderaCount4  = 1;
+		turderaAudio4.play();
+		turderaPlayPauseBTN4.innerHTML = "||";
+	}else{
+		turderaCount4 = 0;
+		turderaAudio4.pause();
+		turderaPlayPauseBTN4.innerHTML = "►";
+	}
+}
+ 
+function turderaStop4(){
+	turderaPlayPause4()
+	turderaAudio4.pause();
+	turderaAudio4.currentTime = 0;
+	turderaPlayPauseBTN4.innerHTML = "►";
+}
+
+/* -------------------------------------------------------------- */
+
+var turderaAudio5 = document.getElementById('turderaAudio5');
+var turderaPlayPauseBTN5 = document.getElementById('turderaPlayPauseBTN5');
+var turderaCount5 = 0;
+
+function turderaPlayPause5(){
+	if(turderaCount5  == 0){
+		turderaCount5  = 1;
+		turderaAudio5.play();
+		turderaPlayPauseBTN5.innerHTML = "||";
+	}else{
+		turderaCount5 = 0;
+		turderaAudio5.pause();
+		turderaPlayPauseBTN5.innerHTML = "►";
+	}
+}
+ 
+function turderaStop5(){
+	turderaPlayPause5()
+	turderaAudio5.pause();
+	turderaAudio5.currentTime = 0;
+	turderaPlayPauseBTN5.innerHTML = "►";
+}
+
+/* -------------------------------------------------------------- */
+
+var turderaAudio6 = document.getElementById('turderaAudio6');
+var turderaPlayPauseBTN6 = document.getElementById('turderaPlayPauseBTN6');
+var turderaCount6 = 0;
+
+function turderaPlayPause6(){
+	if(turderaCount6  == 0){
+		turderaCount6  = 1;
+		turderaAudio6.play();
+		turderaPlayPauseBTN6.innerHTML = "||";
+	}else{
+		turderaCount6 = 0;
+		turderaAudio6.pause();
+		turderaPlayPauseBTN6.innerHTML = "►";
+	}
+}
+ 
+function turderaStop6(){
+	turderaPlayPause6()
+	turderaAudio6.pause();
+	turderaAudio6currentTime = 0;
+	turderaPlayPauseBTN6.innerHTML = "►";
+}
+
+/* -------------------------------------------------------------- */
+
+var turderaAudio7 = document.getElementById('turderaAudio7');
+var turderaPlayPauseBTN7 = document.getElementById('turderaPlayPauseBTN7');
+var turderaCount7 = 0;
+
+function turderaPlayPause7(){
+	if(turderaCount7  == 0){
+		turderaCount7  = 1;
+		turderaAudio7.play();
+		turderaPlayPauseBTN7.innerHTML = "||";
+	}else{
+		turderaCount7 = 0;
+		turderaAudio7.pause();
+		turderaPlayPauseBTN7.innerHTML = "►";
+	}
+}
+ 
+function turderaStop7(){
+	turderaPlayPause7()
+	turderaAudio7.pause();
+	turderaAudio7.currentTime = 0;
+	turderaPlayPauseBTN7.innerHTML = "►";
+}
+
+/* -------------------------------------------------------------- */
+
+var turderaAudio8 = document.getElementById('turderaAudio8');
+var turderaPlayPauseBTN8 = document.getElementById('turderaPlayPauseBTN8');
+var turderaCount8 = 0;
+
+function turderaPlayPause8(){
+	if(turderaCount8  == 0){
+		turderaCount8  = 1;
+		turderaAudio8.play();
+		turderaPlayPauseBTN8.innerHTML = "||";
+	}else{
+		turderaCount8 = 0;
+		turderaAudio8.pause();
+		turderaPlayPauseBTN8.innerHTML = "►";
+	}
+}
+ 
+function turderaStop8(){
+	turderaPlayPause8()
+	turderaAudio8.pause();
+	turderaAudio8.currentTime = 0;
+	turderaPlayPauseBTN8.innerHTML = "►";
+}
+
+/* -------------------------------------------------------------- */
+
+var turderaAudio9 = document.getElementById('turderaAudio9');
+var turderaPlayPauseBTN9 = document.getElementById('turderaPlayPauseBTN9');
+var turderaCount9 = 0;
+
+function turderaPlayPause9(){
+	if(turderaCount9  == 0){
+		turderaCount9  = 1;
+		turderaAudio9.play();
+		turderaPlayPauseBTN9.innerHTML = "||";
+	}else{
+		turderaCount9 = 0;
+		turderaAudio9.pause();
+		turderaPlayPauseBTN9.innerHTML = "►";
+	}
+}
+ 
+function turderaStop9(){
+	turderaPlayPause9()
+	turderaAudio9.pause();
+	turderaAudio9.currentTime = 0;
+	turderaPlayPauseBTN9.innerHTML = "►";
+}
